0b8e6f16a0439098b4d4a9d02bf184cee2f3b0df
Celeter <alansky114@gmail.com>
2020-01-02 18:38:15 +0800

    up

---
+++ assets/js/init.js
@@ -0,0 +1,52 @@
+/*-----------------------------------------------------------------------------------*/
+/*  DOCUMENT READY
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function(){
+    'use strict';
+    // ANIMATED ONLY IF NOT AT TOP OF PAGE ON LOAD //
+    var $win = $(window);
+    if ($win.scrollTop() == 0)
+        jQuery('.navbar-fixed-top').addClass('wow');
+    else if ($win.height() + $win.scrollTop() == $(document).height()) {
+         jQuery('.navbar-fixed-top').removeClass('wow');
+    }
+
+    //NEAT VID EMBEDS
+    $().prettyEmbed({ useFitVids: true });
+    $('.vertical-center').flexVerticalCenter({ cssAttribute: 'padding-top' });
+
+    // ONEPAGER XTRA //
+    //$('body').scrollspy({
+    //    target: '.navbar-fixed-top'
+    //});
+
+    // FULLSCREEN FIX //
+    var windowHeight = $(window).innerHeight();
+    var isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if( !isMobileDevice ) {
+        $('#headerwrap.fullheight').css('height', windowHeight);
+        $(window).resize(function() {
+            $('#headerwrap.fullheight').css('height', windowHeight);
+        });
+    }
+
+    // ANIMATE ONCE PAGE LOAD IS OVER //
+    Pace.on("done", function(){
+        new WOW().init();
+        $(window).trigger('resize');
+    });
+});
+
+function load_README() {
+    $.get("Rule/README.md", function (response, status, xhr) {
+        var converter = new showdown.Converter();
+        $("#content").html(converter.makeHtml(response));
+    });
+}
+
+function load_Subscribe() {
+    $.get("Rule/Subscribe.md", function (response, status, xhr) {
+        var converter = new showdown.Converter();
+        $("#content").html(converter.makeHtml(response));
+    });
+}
